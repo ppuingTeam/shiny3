a5960944018f5ae4ddeae71e9540796a4ed62956
Byeongkap Cho <yschem@yschem.local>
2015-02-10 16:54:45 +0900

    Initial Commit

---
+++ test3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test3
+//
+//  Created by Byeongkap Cho on 2015. 2. 10..
+//  Copyright (c) 2015년 shiny14days. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test3/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test3
+//
+//  Created by Byeongkap Cho on 2015. 2. 10..
+//  Copyright (c) 2015년 shiny14days. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
